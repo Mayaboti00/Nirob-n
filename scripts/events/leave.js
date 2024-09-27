ce7e82c819a29481c3278bb522c01f4dd887bfa5
Mayaboti00 <nirobkkcc@gmail.com>
2024-09-27 13:06:58 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -20,7 +20,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "_🦋🍒 প্রিয় {name} তুমি জে এই গ্রুপে থাকার যোগ্য না সেটা আমরা ভালো করে জানি...!😧🍼.\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
